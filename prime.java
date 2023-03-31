fd2aaac91202be76953d373ab5505530a97f9401
Manjari Srivastav <126674100+manjarisri@users.noreply.github.com>
2023-03-31 15:11:11 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -24,7 +24,7 @@
 //     }
 
 
-    public class PrimeExample{    
+    public class prime{    
      public static void main(String args[]){    
       int i,m=0,flag=0;      
       int n=3;//it is the number to be checked    
