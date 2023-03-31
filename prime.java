6a6d31231bc1c2e6780509f58bb0988f6678f7e4
Manjari Srivastav <126674100+manjarisri@users.noreply.github.com>
2023-03-31 17:51:52 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -3,8 +3,8 @@
         public static void main(String[] args) {
             Scanner s = new Scanner(System.in);
             System.out.print("Enter a number : ");
-           //int n = s.nextInt();
-           int n;
+           int n = s.nextInt();
+           //int n;
             if (isPrime(n)) {
                 System.out.println(n + " is a prime number");
             } else {
